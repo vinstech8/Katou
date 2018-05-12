b6a652ef9a844cf71e0f8b15889fec58a7b53a51
Vins <vinstech8@yahoo.com>
2018-05-12 18:51:20 -0400

    added index with comments. are you happy mom

---
+++ index.js
@@ -0,0 +1,43 @@
+// ? Do these imports properly function?
+import commando from "discord.js-commando";
+import config from "./config.json";
+
+const client = new commando.Client({
+    owner: config.owner,
+    commandPrefix: config.prefix
+});
+
+// * Core Event Handling
+client.on('ready', () => { // Startup
+    console.log(`Katou is online on ${client.guilds.size} guilds.`);
+    client.user.setActivity("the stock market | k!", { type: 'WATCHING'});
+});
+client.on('message', () => {
+    // TODO: Add in more message handling
+});
+
+// * Guild Events
+client.on('guildCreate', guild => { // Joined Guild
+    console.log(`Katou has joined ${guild.name}. ID: ${guild.id}`);
+});
+client.on('guildDelete', guild => { // Left Guild
+    console.log(`Katou has left ${guild.name}. ID: ${guild.id}`);
+});
+
+// * Error Handling
+client.on('error', (e) => console.error(e));
+client.on('warn', (e) => console.warn(e));
+client.on('debug', (e) => console.info(e));
+
+// * Command Registry
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['basic', 'Basic commands']
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(`${__dirname}/commands`);
+
+// * Magic!
+client.login(config.token);
